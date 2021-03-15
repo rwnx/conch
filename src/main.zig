ce031c24046124c09e1e28920283b42b140739dd
Jerome Twell <jtwell1@gmail.com>
2021-03-15 02:11:44 +0000

    use ArrayList like flexible buffer in

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,48 @@
 const std = @import("std");
 const eql = std.mem.eql;
-const stdin = std.io.getStdIn().inStream();
-const stdout = std.io.getStdOut().outStream();
+const stdin = std.io.getStdIn().reader();
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
+const print = @import("std").debug.print;
+
+const page_allocator = std.heap.page_allocator;
+const ArenaAllocator = std.heap.ArenaAllocator;
+const ArrayList = std.ArrayList;
+
+const Allocator = std.mem.Allocator;
+
+fn get_line(allocator: *Allocator) ![]u8 {
+    var list = ArrayList(u8).init(allocator);
+
+    while (true) {
+        const byte = stdin.readByte() catch |err| switch (err) {
+            error.EndOfStream => {
+                break;
+            },
+            else => |e| return e,
+        };
+
+        if (byte == '\n') {
+            break;
+        }
+
+        try list.append(byte);
+    }
+
+    return list.items;
+}
 
 pub fn main() !void {
-    var buf: [10]u8 = undefined;
+  
+    while (true) {
+        var arena = ArenaAllocator.init(page_allocator);
+        defer arena.deinit();
+        const allocator = &arena.allocator;
 
-    while(true) {
-      try stdout.print("> ", .{});
+        try stdout.print("> ", .{});
 
-      const line = try stdin.readUntilDelimiterOrEof(&buf, '\n');
-      
-      try stdout.print("\"{}\"\n", .{line});
+        const line = get_line(allocator) catch "";
+
+        print("{}\n", .{line});
     }
 }
